a5528536cf96fa32697395b7b33285a708456270
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-08-29 22:20:02 +0300

    render navlink

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,45 +1,53 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './Navbar.css'
+import {links} from "../../assets/data";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
 
             <ul className="navbar-list">
-                <Link to='/'>
-                    <li className="navbar-item">
-                        <button className="navbar-link  active" data-nav-link="data-nav">Обо мне</button>
-                    </li>
-                </Link>
+                {
+                   links.map(({name, path}, index) => {
+                       return (
+                           <Link key={index} to={path}>
+                               <li className="navbar-item">
+                                   <button className="navbar-link  active" data-nav-link="data-nav">{name}</button>
+                               </li>
+                           </Link>
+                       )
+                   })
+                }
 
 
-                <Link to='/resume'>
-                    <li className="navbar-item">
-                        <button className="navbar-link" data-nav-link="data-nav">Резюме</button>
-                    </li>
-                </Link>
+
+                {/*<Link to='/resume'>*/}
+                {/*    <li className="navbar-item">*/}
+                {/*        <button className="navbar-link" data-nav-link="data-nav">Резюме</button>*/}
+                {/*    </li>*/}
+                {/*</Link>*/}
 
 
-                <Link to='/portfolio'>
-                    <li className="navbar-item">
-                        <button className="navbar-link" data-nav-link="data-nav">Портфолио</button>
-                    </li>
-                </Link>
+                {/*<Link to='/portfolio'>*/}
+                {/*    <li className="navbar-item">*/}
+                {/*        <button className="navbar-link" data-nav-link="data-nav">Портфолио</button>*/}
+                {/*    </li>*/}
+                {/*</Link>*/}
 
 
-                <Link to='/blog'>
-                    <li className="navbar-item">
-                        <button className="navbar-link" data-nav-link="data-nav">Блог</button>
-                    </li>
-                </Link>
+                {/*<Link to='/blog'>*/}
+                {/*    <li className="navbar-item">*/}
+                {/*        <button className="navbar-link" data-nav-link="data-nav">Блог</button>*/}
+                {/*    </li>*/}
+                {/*</Link>*/}
 
 
-                <Link to='/contact'>
-                    <li className="navbar-item">
-                        <button className="navbar-link" data-nav-link="data-nav">Контакты</button>
-                    </li>
-                </Link>
+                {/*<Link to='/contact'>*/}
+                {/*    <li className="navbar-item">*/}
+                {/*        <button className="navbar-link" data-nav-link="data-nav">Контакты</button>*/}
+                {/*    </li>*/}
+                {/*</Link>*/}
 
             </ul>
 
