d9493f83b641f09f052ca1032dc14daa2a047747
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-08-29 22:12:14 +0300

    create data.js

---
+++ src/assets/data.js
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        name: "Обо мне",
+        path: '/'
+    },
+    {
+        name: "Резюме",
+        path: '/resume'
+    },
+    {
+        name: "Портфолио",
+        path: '/portfolio'
+    },
+    {
+        name: "Блог",
+        path: '/blog'
+    },
+    {
+        name: "Контакты",
+        path: '/contact'
+    }
+]
