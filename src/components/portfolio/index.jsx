e81e877bd6987db5324c654df2b32dcb40d9b19e
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-09-03 16:32:16 +0300

    pagination component

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -4,6 +4,7 @@ import Header from "../Header";
 import Category from "./section/Category";
 import Project from "./section/Project";
 import Search from "./section/Search";
+import Pagination from "../pagination";
 
 
 //https://62da700d9eedb699636e2d90.mockapi.io/projects
@@ -11,16 +12,17 @@ const Portfolio = () => {
     const [categoryId, setCategoryId] = React.useState(0);
     const [projects, setProjects] =React.useState([]);
     const [search, setSearch] = React.useState('');
+    const [currentPage, setCurrentPage] = React.useState(1)
 
 
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const searchValue = search ? `&search=${search}` : '';
     React.useEffect(() =>{
-        fetch(`https://62da700d9eedb699636e2d90.mockapi.io/projects?${category}${searchValue}`)
+        fetch(`https://62da700d9eedb699636e2d90.mockapi.io/projects?page=${currentPage}&limit=4&${category}${searchValue}`)
             .then((response) => response.json())
             .then((items)=>{setProjects(items);
         });
-    }, [categoryId, search]);
+    }, [categoryId, search, currentPage]);
     return (
         <div className="portfolio" data-page="portfolio">
 
@@ -37,6 +39,7 @@ const Portfolio = () => {
                     {projects.map(obj => (<Project key={obj.id} {...obj}/>))}
                 </ul>
 
+                <Pagination currentPage={currentPage} onChangePage={(page)=>setCurrentPage(page)}/>
             </section>
 
         </div>
