b7d8ded3be26ad9a0f8ae9ee79d333d5e95763bd
GarryProhor <84273694+GarryProhor@users.noreply.github.com>
2022-09-07 14:00:00 +0300

    form component

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -7,197 +7,202 @@ import blog4 from "../../assets/images/blog-4.jpg";
 import blog5 from "../../assets/images/blog-5.jpg";
 import blog6 from "../../assets/images/blog-6.jpg";
 import Header from "../Header";
+import Login from "./login";
 
 const Blog = () => {
     return (
-        <div className="blog" data-page="blog">
+        <>
+            <div className="blog" data-page="blog">
 
-            <Header title='Блог'/>
+                <Header title='Блог'/>
 
-            <section className="blog-posts">
+                <Login />
 
-                <ul className="blog-posts-list">
+                {/*<section className="blog-posts">*/}
 
-                    <li className="blog-post-item">
-                        <a href="src/components/blog/index.jsx">
+                {/*    <ul className="blog-posts-list">*/}
 
-                            <figure className="blog-banner-box">
-                                <img src={blog1} alt="Design conferences in 2022"
-                                     loading="lazy"/>
-                            </figure>
+                {/*        <li className="blog-post-item">*/}
+                {/*            <a href="src/components/blog/index.jsx">*/}
 
-                            <div className="blog-content">
+                {/*                <figure className="blog-banner-box">*/}
+                {/*                    <img src={blog1} alt="Design conferences in 2022"*/}
+                {/*                         loading="lazy"/>*/}
+                {/*                </figure>*/}
 
-                                <div className="blog-meta">
-                                    <p className="blog-category">Дизайн</p>
+                {/*                <div className="blog-content">*/}
 
-                                    <span className="dot"></span>
+                {/*                    <div className="blog-meta">*/}
+                {/*                        <p className="blog-category">Дизайн</p>*/}
 
-                                    <time dateTime="2022-02-23">Февраль 23, 2022</time>
-                                </div>
+                {/*                        <span className="dot"></span>*/}
 
-                                <h3 className="h3 blog-item-title">Конференций по дизайну в 2022 году</h3>
+                {/*                        <time dateTime="2022-02-23">Февраль 23, 2022</time>*/}
+                {/*                    </div>*/}
 
-                                <p className="blog-text">
-                                    Veritatis et quasi architecto beatae vitae dicta sunt, explicabo.
-                                </p>
+                {/*                    <h3 className="h3 blog-item-title">Конференций по дизайну в 2022 году</h3>*/}
 
-                            </div>
+                {/*                    <p className="blog-text">*/}
+                {/*                        Veritatis et quasi architecto beatae vitae dicta sunt, explicabo.*/}
+                {/*                    </p>*/}
 
-                        </a>
-                    </li>
+                {/*                </div>*/}
 
-                    <li className="blog-post-item">
-                        <a href="src/components/blog/index.jsx">
+                {/*            </a>*/}
+                {/*        </li>*/}
 
-                            <figure className="blog-banner-box">
-                                <img src={blog2} alt="Best fonts every designer"
-                                     loading="lazy"/>
-                            </figure>
+                {/*        <li className="blog-post-item">*/}
+                {/*            <a href="src/components/blog/index.jsx">*/}
 
-                            <div className="blog-content">
+                {/*                <figure className="blog-banner-box">*/}
+                {/*                    <img src={blog2} alt="Best fonts every designer"*/}
+                {/*                         loading="lazy"/>*/}
+                {/*                </figure>*/}
 
-                                <div className="blog-meta">
-                                    <p className="blog-category">Дизайн</p>
+                {/*                <div className="blog-content">*/}
 
-                                    <span className="dot"></span>
+                {/*                    <div className="blog-meta">*/}
+                {/*                        <p className="blog-category">Дизайн</p>*/}
 
-                                    <time dateTime="2022-02-23">Февраль 23, 2022</time>
-                                </div>
+                {/*                        <span className="dot"></span>*/}
 
-                                <h3 className="h3 blog-item-title">Лучшие шрифты каждого дизайнера</h3>
+                {/*                        <time dateTime="2022-02-23">Февраль 23, 2022</time>*/}
+                {/*                    </div>*/}
 
-                                <p className="blog-text">
-                                    Sed ut perspiciatis, nam libero tempore, cum soluta nobis est eligendi.
-                                </p>
+                {/*                    <h3 className="h3 blog-item-title">Лучшие шрифты каждого дизайнера</h3>*/}
 
-                            </div>
+                {/*                    <p className="blog-text">*/}
+                {/*                        Sed ut perspiciatis, nam libero tempore, cum soluta nobis est eligendi.*/}
+                {/*                    </p>*/}
 
-                        </a>
-                    </li>
+                {/*                </div>*/}
 
-                    <li className="blog-post-item">
-                        <a href="src/components/blog/index.jsx">
+                {/*            </a>*/}
+                {/*        </li>*/}
 
-                            <figure className="blog-banner-box">
-                                <img src={blog3} alt="Design digest #80" loading="lazy"/>
-                            </figure>
+                {/*        <li className="blog-post-item">*/}
+                {/*            <a href="src/components/blog/index.jsx">*/}
 
-                            <div className="blog-content">
+                {/*                <figure className="blog-banner-box">*/}
+                {/*                    <img src={blog3} alt="Design digest #80" loading="lazy"/>*/}
+                {/*                </figure>*/}
 
-                                <div className="blog-meta">
-                                    <p className="blog-category">Дизайн</p>
+                {/*                <div className="blog-content">*/}
 
-                                    <span className="dot"></span>
+                {/*                    <div className="blog-meta">*/}
+                {/*                        <p className="blog-category">Дизайн</p>*/}
 
-                                    <time dateTime="2022-02-23">Дизайн 23, 2022</time>
-                                </div>
+                {/*                        <span className="dot"></span>*/}
 
-                                <h3 className="h3 blog-item-title">Дайджест дизайна #80</h3>
+                {/*                        <time dateTime="2022-02-23">Дизайн 23, 2022</time>*/}
+                {/*                    </div>*/}
 
-                                <p className="blog-text">
-                                    Excepteur sint occaecat cupidatat no proident, quis nostrum exercitationem
-                                    ullam corporis suscipit.
-                                </p>
+                {/*                    <h3 className="h3 blog-item-title">Дайджест дизайна #80</h3>*/}
 
-                            </div>
+                {/*                    <p className="blog-text">*/}
+                {/*                        Excepteur sint occaecat cupidatat no proident, quis nostrum exercitationem*/}
+                {/*                        ullam corporis suscipit.*/}
+                {/*                    </p>*/}
 
-                        </a>
-                    </li>
+                {/*                </div>*/}
 
-                    <li className="blog-post-item">
-                        <a href="src/components/blog/index.jsx">
+                {/*            </a>*/}
+                {/*        </li>*/}
 
-                            <figure className="blog-banner-box">
-                                <img src={blog4} alt="UI interactions of the week"
-                                     loading="lazy"/>
-                            </figure>
+                {/*        <li className="blog-post-item">*/}
+                {/*            <a href="src/components/blog/index.jsx">*/}
 
-                            <div className="blog-content">
+                {/*                <figure className="blog-banner-box">*/}
+                {/*                    <img src={blog4} alt="UI interactions of the week"*/}
+                {/*                         loading="lazy"/>*/}
+                {/*                </figure>*/}
 
-                                <div className="blog-meta">
-                                    <p className="blog-category">Дизайн</p>
+                {/*                <div className="blog-content">*/}
 
-                                    <span className="dot"></span>
+                {/*                    <div className="blog-meta">*/}
+                {/*                        <p className="blog-category">Дизайн</p>*/}
 
-                                    <time dateTime="2022-02-23">Февраль 23, 2022</time>
-                                </div>
+                {/*                        <span className="dot"></span>*/}
 
-                                <h3 className="h3 blog-item-title">UI - неделя</h3>
+                {/*                        <time dateTime="2022-02-23">Февраль 23, 2022</time>*/}
+                {/*                    </div>*/}
 
-                                <p className="blog-text">
-                                    Enim ad minim veniam, consectetur adipiscing elit, quis nostrud exercitation
-                                    ullamco laboris nisi.
-                                </p>
+                {/*                    <h3 className="h3 blog-item-title">UI - неделя</h3>*/}
 
-                            </div>
+                {/*                    <p className="blog-text">*/}
+                {/*                        Enim ad minim veniam, consectetur adipiscing elit, quis nostrud exercitation*/}
+                {/*                        ullamco laboris nisi.*/}
+                {/*                    </p>*/}
 
-                        </a>
-                    </li>
+                {/*                </div>*/}
 
-                    <li className="blog-post-item">
-                        <a href="src/components/blog/index.jsx">
+                {/*            </a>*/}
+                {/*        </li>*/}
 
-                            <figure className="blog-banner-box">
-                                <img src={blog5} alt="The forgotten art of spacing"
-                                     loading="lazy"/>
-                            </figure>
+                {/*        <li className="blog-post-item">*/}
+                {/*            <a href="src/components/blog/index.jsx">*/}
 
-                            <div className="blog-content">
+                {/*                <figure className="blog-banner-box">*/}
+                {/*                    <img src={blog5} alt="The forgotten art of spacing"*/}
+                {/*                         loading="lazy"/>*/}
+                {/*                </figure>*/}
 
-                                <div className="blog-meta">
-                                    <p className="blog-category">Дизайн</p>
+                {/*                <div className="blog-content">*/}
 
-                                    <span className="dot"></span>
+                {/*                    <div className="blog-meta">*/}
+                {/*                        <p className="blog-category">Дизайн</p>*/}
 
-                                    <time dateTime="2022-02-23">Февраль 23, 2022</time>
-                                </div>
+                {/*                        <span className="dot"></span>*/}
 
-                                <h3 className="h3 blog-item-title">Забытое искусство интервалов</h3>
+                {/*                        <time dateTime="2022-02-23">Февраль 23, 2022</time>*/}
+                {/*                    </div>*/}
 
-                                <p className="blog-text">
-                                    Maxime placeat, sed do eiusmod tempor incididunt ut labore et dolore magna
-                                    aliqua.
-                                </p>
+                {/*                    <h3 className="h3 blog-item-title">Забытое искусство интервалов</h3>*/}
 
-                            </div>
+                {/*                    <p className="blog-text">*/}
+                {/*                        Maxime placeat, sed do eiusmod tempor incididunt ut labore et dolore magna*/}
+                {/*                        aliqua.*/}
+                {/*                    </p>*/}
 
-                        </a>
-                    </li>
+                {/*                </div>*/}
 
-                    <li className="blog-post-item">
-                        <a href="src/components/blog/index.jsx">
+                {/*            </a>*/}
+                {/*        </li>*/}
 
-                            <figure className="blog-banner-box">
-                                <img src={blog6} alt="Design digest #79" loading="lazy"/>
-                            </figure>
+                {/*        <li className="blog-post-item">*/}
+                {/*            <a href="src/components/blog/index.jsx">*/}
 
-                            <div className="blog-content">
+                {/*                <figure className="blog-banner-box">*/}
+                {/*                    <img src={blog6} alt="Design digest #79" loading="lazy"/>*/}
+                {/*                </figure>*/}
 
-                                <div className="blog-meta">
-                                    <p className="blog-category">Дизайн</p>
+                {/*                <div className="blog-content">*/}
 
-                                    <span className="dot"></span>
+                {/*                    <div className="blog-meta">*/}
+                {/*                        <p className="blog-category">Дизайн</p>*/}
 
-                                    <time dateTime="2022-02-23">Февраль 23, 2022</time>
-                                </div>
+                {/*                        <span className="dot"></span>*/}
 
-                                <h3 className="h3 blog-item-title">Дайджест дизайна #79</h3>
+                {/*                        <time dateTime="2022-02-23">Февраль 23, 2022</time>*/}
+                {/*                    </div>*/}
 
-                                <p className="blog-text">
-                                    Optio cumque nihil impedit uo minus quod maxime placeat, velit esse cillum.
-                                </p>
+                {/*                    <h3 className="h3 blog-item-title">Дайджест дизайна #79</h3>*/}
 
-                            </div>
+                {/*                    <p className="blog-text">*/}
+                {/*                        Optio cumque nihil impedit uo minus quod maxime placeat, velit esse cillum.*/}
+                {/*                    </p>*/}
 
-                        </a>
-                    </li>
+                {/*                </div>*/}
 
-                </ul>
+                {/*            </a>*/}
+                {/*        </li>*/}
 
-            </section>
+                {/*    </ul>*/}
 
-        </div>
+                {/*</section>*/}
+
+            </div>
+        </>
     );
 };
 
---
+++ src/components/blog/login/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Login.css'
+import Login from "./Login";
+import Registration from "./Registration";
+
+const Form = () => {
+    return (
+
+        <>
+            <div className="main">
+
+
+                <Login />
+                {/*<Registration />*/}
+
+
+            </div>
+
+        </>
+
+    );
+};
+
+export default Form;
